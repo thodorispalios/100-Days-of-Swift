275af085b769c9ba67525768599e1574ef9ea5f2
Theo <thodorispalios1@hotmail.com>
2021-02-06 14:32:56 +0200

    w

---
+++ WordScramble/Shared/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  Shared
+//
+//  Created by Theo on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
