9cf4b750c61750897ddf65e293eafc4f82f6b3aa
Theo <thodorispalios1@hotmail.com>
2021-03-09 18:11:11 +0200

    iExpence
    iExpence Project Added

---
+++ iExpence/Shared/iExpenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenceApp.swift
+//  Shared
+//
+//  Created by Theo on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
