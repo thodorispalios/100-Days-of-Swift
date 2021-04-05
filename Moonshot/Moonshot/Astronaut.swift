6cf81261bffa38527386738590a2951fea524863
Theo <thodorispalios1@hotmail.com>
2021-04-05 14:05:02 +0300

    Completed Moonshot

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Theo on 4/4/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,39 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Theo on 4/4/21.
+//
+
+import Foundation
+
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo \(id)"
+    }
+    
+    var formatedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+}
