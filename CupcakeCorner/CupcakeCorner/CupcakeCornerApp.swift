29d486284e681f91cdc37f90a08105efdcbc671b
Theo <thodorispalios1@hotmail.com>
2021-04-06 18:36:25 +0300

    Created Cupcake Corner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Theo on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
